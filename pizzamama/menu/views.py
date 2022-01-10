1a25bfddd8f1996e6e31eee5f95c49cbdb0e4abd
Nick Armenis <armenisnick@gmail.com>
2022-01-10 19:02:44 +0200

    RestAPI

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from .models import Pizza
 
 #Create views Here
@@ -11,3 +12,9 @@ def index(request):
     return HttpResponse("Our Pizzas: "+ pizzas_names_and_price_str)'''
     pizzas = Pizza.objects.all()
     return render(request, 'menu/index.html', {'pizzas': pizzas})
+
+
+def api_get_pizzas(request):
+    pizzas = Pizza.objects.all().order_by('price')
+    json=serializers.serialize("json",pizzas)
+    return HttpResponse(json)
