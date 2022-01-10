587bdb2126638953bfecf8cb1245d3edc2955bcc
Nick Armenis <armenisnick@gmail.com>
2022-01-10 19:52:50 +0200

    Contact Page

--- pizzamama/pizzamama/settings.py
+++ pizzamama/pizzamama/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'menu.apps.MenuConfig',
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
+    'contact.apps.ContactConfig'
 ]
 
 MIDDLEWARE = [
--- pizzamama/pizzamama/urls.py
+++ pizzamama/pizzamama/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('menu/', include('menu.urls')),
+    path('piza-menu/', include('menu.urls')),
     path('api/', include('menu.api_urls')),
     path('', include('main.urls')),
+    path('contact/', include('contact.urls')),
 ]
